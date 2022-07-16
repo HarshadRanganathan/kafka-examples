29b0e0c740681496900df2bdbb23ab81819a928f
HarshadRanganathan <rharshad93@gmail.com>
2022-07-16 22:14:47 +0100

    Kafka consumer

---
+++ spring-kafka/src/main/java/com/springkafka/example/kafka/consumer/PaymentConsumer.java
@@ -0,0 +1,32 @@
+package com.springkafka.example.kafka.consumer;
+
+import example.avro.Payment;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentConsumer {
+
+    private Payment payment;
+
+    @KafkaListener(topics = "payments", groupId = "consumer-group-1")
+    public void consume(final ConsumerRecord<?, ?> consumerRecord) {
+        log.info("Received payload='{}'", consumerRecord.value());
+
+        final Payment p = (Payment) consumerRecord.value();
+        setPayment(p);
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    private void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+}
